881b2e20384c1189f38d5ee666480c3e7f0d60ce
emsroxbotix <emsroxbotix@gmail.com>
2016-11-28 15:56:24 -0500

    Add files via upload

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutoTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by steamuser on 11/28/2016.
+ */
+@Autonomous(name = "AutoTest", group = "EmsRoxbotix1")
+public class AutoTest {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    DcMotor spinner;
+
+
+    public void init() {
+
+
+    }
+
+}
